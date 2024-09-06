0a0b26983d682d6ee63d4007388fc6b5b1479afa
feliperbdantas <feliperbdantas@outlook.com>
2024-09-05 21:49:48 -0300

    Design System in React - Stitches Configuration

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,31 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
