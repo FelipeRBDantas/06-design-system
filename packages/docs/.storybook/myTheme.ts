decd1b52ddbdf9147dc7b8bae63a0e5d29c11084
feliperbdantas <feliperbdantas@outlook.com>
2024-09-14 21:14:55 -0300

    Design System in React - Theme Dark in Storybook

--- packages/docs/.storybook/myTheme.ts
+++ packages/docs/.storybook/myTheme.ts
@@ -6,29 +6,29 @@ export default create({
   fontBase: '"Source Sans Pro", sans-serif',
   fontCode: 'monospace',
 
-  brandTitle: 'Forest UI',
-  brandUrl: 'https://example.com',
+  brandTitle: 'Ignite UI',
+  brandUrl: 'https://example.com.br',
   brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
   brandTarget: '_self',
 
-  colorPrimary: '#00FF85', // Verde vibrante
-  colorSecondary: '#007F5F', // Verde floresta
+  colorPrimary: '#00FF85',
+  colorSecondary: '#007F5F',
 
-  appBg: '#002B36', // Fundo do app verde escuro
+  appBg: '#002B36',
   appContentBg: '#002B36',
   appPreviewBg: '#002B36',
-  appBorderColor: '#007F5F', // Borda verde escura
+  appBorderColor: '#007F5F',
   appBorderRadius: 8,
 
-  textInverseColor: '#E6FFFA', // Texto claro sobre fundo escuro
+  textInverseColor: '#E6FFFA',
   textColor: '#FFFFFF',
 
   barTextColor: '#FFFFFF',
-  barSelectedColor: '#00FF85', // Barra selecionada verde
-  barHoverColor: '#007F5F', // Cor da barra ao passar o mouse
-  barBg: '#004D40', // Fundo da barra verde escuro
+  barSelectedColor: '#00FF85',
+  barHoverColor: '#007F5F',
+  barBg: '#004D40',
 
-  inputBg: '#004D40', // Fundo dos inputs verde escuro
+  inputBg: '#004D40',
   inputBorder: '#007F5F',
   inputTextColor: '#FFFFFF',
   inputBorderRadius: 5,
