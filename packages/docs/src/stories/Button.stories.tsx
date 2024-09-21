93942174deaac8c00889319f23bc484d14248d72
feliperbdantas <feliperbdantas@outlook.com>
2024-09-21 18:55:56 -0300

    feat: Design System Components - Components: Button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -10,3 +10,50 @@ export default {
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create New',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, the button always renders as `primary`, but we can change this with the `variant` property. In this case, set to `secondary`. Typically used for secondary actions.',
+      },
+    },
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, the button always renders as `primary`, but we can change this with the `variant` property. In this case, set to `tertiary`. Typically used for cancel button patterns.',
+      },
+    },
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, the button always renders as `md`, but we can change this with the `size` property. In this case, set it to `sm` for smaller sizes.',
+      },
+    },
+  },
+}
--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -4,7 +4,62 @@ import { VariantProps } from '@stitches/react'
 
 import { styled } from '../styles'
 
-export const Button = styled('button', {})
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+  padding: '0 $4',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$ignite500',
+      },
+
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+      },
+
+      tertiary: {
+        color: '$gray100',
+      },
+    },
+
+    size: {
+      sm: {
+        height: 38,
+      },
+
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
 
 export interface ButtonProps
   extends ComponentProps<typeof Button>,
